c8eb813e135163b11842b9c411a5ec1dd5cab520
brendanrauch <brendanjrauch@gmail.com>
2023-09-28 21:01:11 -0600

    done

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -3,18 +3,33 @@ const button = document.querySelector('button');
 const list = document.querySelector('#list');
 const feedback = document.querySelector('#feedback');
 
+
 button.addEventListener('click', () => {
     if (input.value) {
         //temp:
         console.log('clicked');
+        const li = document.createElement('li');
+        const deleteButton = document.createElement('button');
 
         feedback.textContent = "";
+        li.textContent = input.value;
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        deleteButton.addEventListener('click', function () {
+            list.removeChild(li);
+            input.focus();
+        })
+        input.focus();
+        input.value='';
     }
+
     else {
         input.focus();
-
+       
         //give feedback to user
         feedback.textContent = "Please enter a Book and Chapter"
     }
 });
+
 
