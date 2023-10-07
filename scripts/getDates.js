70e473c25ea353816f9f321d9772e7ebf9885d90
brendanrauch <brendanjrauch@gmail.com>
2023-10-07 13:38:45 -0600

    dark mode

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -2,3 +2,25 @@ document.getElementById("currentYear").innerHTML = new Date().getFullYear();
 
 document.getElementById("lastModified").innerHTML = document.lastModified;
 
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
+
+const modeButton = document.querySelector("#mode");
+const main = document.querySelector("main");
+
+modeButton.addEventListener("click", () => {
+	if (modeButton.textContent.includes("☑️")) {
+		main.style.background = "#000";
+		main.style.color = "#fff";
+		modeButton.textContent = "❎";
+	} else {
+		main.style.background = "#eee";
+		main.style.color = "#000";
+		modeButton.textContent = "☑️";
+	}
+});
