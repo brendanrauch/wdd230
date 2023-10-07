449495e9d4c9c9277c42b5527230f01764744ee6
brendanrauch <brendanjrauch@gmail.com>
2023-10-07 15:05:03 -0600

    done

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,12 @@
+
+document.getElementById("currentYear").innerHTML = new Date().getFullYear();
+
+document.getElementById("lastModified").innerHTML = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
