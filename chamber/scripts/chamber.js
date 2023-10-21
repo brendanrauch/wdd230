24b93df1f5e6b4edf1f5e376281bfd799955cf9f
brendanrauch <brendanjrauch@gmail.com>
2023-10-21 17:43:06 -0600

    c

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -10,3 +10,30 @@ hamButton.addEventListener('click', () => {
 	navigation.classList.toggle('open');
 	hamButton.classList.toggle('open');
 });
+
+
+const visit = document.querySelector(".visit");
+
+let numVisits = Number(window.localStorage.getItem("visitls"));
+let lastVisit = Number(window.localStorage.getItem("lastVisit"));
+
+const timeBetween = Math.round((Date.now() - lastVisit) / (1000 * 60 *60 * 24));
+
+if (numVisits !==0)
+	if (timeBetween <  1) {
+		visit.textContent = "Back so soon! Awesome!"
+	}
+	else {
+		if (timeBetween = 1) {
+			visit.textContent = "You last visited " + timeBetween + " day ago."
+		}
+		else {
+			visit.textContent = "You last visited " + timeBetween + " days ago."
+		}		
+	}
+else {
+	visit.textContent = "Welcome! Let us know if you have any questions."
+}
+numVisits++;
+localStorage.setItem("lastVisit", Date.now());
+localStorage.setItem("visitls", numVisits);
