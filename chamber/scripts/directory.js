ce7006707d28e66ae004bf6798e8a2e395254a25
brendanrauch <brendanjrauch@gmail.com>
2023-11-18 22:05:09 -0700

    th

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -1,5 +1,5 @@
 
-const url = '/data/directory.json';
+const url = 'data/members.json';
 const cards = document.querySelector('#cards');
 
 const getMembers = (members) => {
@@ -8,29 +8,32 @@ const getMembers = (members) => {
         let card = document.createElement('section');
         let name = document.createElement('h2');
         let address = document.createElement('p');
+        let addressm = document.createElement('p');
         let phone = document.createElement('p');
         let urlm = document.createElement('p');
         let membershiplevel = document.createElement('p');
         let imageurl = document.createElement('img');
     
 
-        name.textContent = `name: ${members.name}`;
-        address.textContent = `address: ${members.address}`;
-        phone.textContent = `phone: ${members.phone}`;
-        urlm.textContent = `url: ${members.url}`;
-        membershiplevel.textContent = `membershiplevel: ${members.memberShipLevel}`;
+        name.textContent = `Name: ${member.name}`;
+        address.textContent = `Address: ${member.address.street1} ${member.address.street2} `;
+        addressm.textContent = ` ${member.address.city} ${member.address.state} ${member.address.zip} `;
+        phone.textContent = `Phone: ${member.phone}`;
+        urlm.textContent = `Url: ${member.url}`;
+        membershiplevel.textContent = `Membership Level: ${member.memberShipLevel}`;
 
 
         // Build the image portrait by setting all the relevant attributes
-        portrait.setAttribute('src', member.imageurl);
-        portrait.setAttribute('alt', `Company Logo`); // fill in the blank
-        portrait.setAttribute('loading', 'lazy');
-        portrait.setAttribute('width', '340');
-        portrait.setAttribute('height', '440');
+        imageurl.setAttribute('src', member.imageUrl);
+        imageurl.setAttribute('alt', `Company Logo`); // fill in the blank
+        imageurl.setAttribute('loading', 'lazy');
+        imageurl.setAttribute('width', '160');
+       
     
         // Append the section(card) with the created elements
         card.appendChild(name);
         card.appendChild(address); 
+        card.appendChild(addressm);
         card.appendChild(phone);
         card.appendChild(urlm);
         card.appendChild(membershiplevel);
@@ -48,7 +51,27 @@ async function getMemberData() {
     const data = await response.json();
     // console.log(data);
     //console.table(data.prophets);
-    getMembers(data.member); // no
+    getMembers(data.members);
 }
   
   getMemberData();
+
+
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+// The following code could be written cleaner. How? We may have to simplfiy our HTMl and think about a default view.
+
+gridbutton.addEventListener("click", () => {
+	// example using arrow function
+	display.classList.add("grid");
+	display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+	display.classList.add("list");
+	display.classList.remove("grid");
+}
